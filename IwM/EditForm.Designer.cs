730222b792f768baa0490466c8992ed470da609a
adzina <adzina3@vp.pl>
2018-06-02 19:32:01 +0200

    EditForm displays info

--- IwM/EditForm.Designer.cs
+++ IwM/EditForm.Designer.cs
@@ -28,9 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.titleLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.editFormBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.editFormBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editFormBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editFormBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -38,13 +45,15 @@
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.351551F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 93.64845F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 42F));
             this.tableLayoutPanel1.Controls.Add(this.titleLabel, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 1);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(698, 264);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
@@ -55,10 +64,23 @@
             this.titleLabel.Location = new System.Drawing.Point(101, 50);
             this.titleLabel.Margin = new System.Windows.Forms.Padding(60, 50, 60, 50);
             this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(496, 32);
+            this.titleLabel.Size = new System.Drawing.Size(494, 22);
             this.titleLabel.TabIndex = 0;
             this.titleLabel.Text = "label1";
             this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 125);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(608, 116);
+            this.dataGridView1.TabIndex = 1;
+
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
             // 
             // EditForm
             // 
@@ -70,6 +92,9 @@
             this.Text = "EditForm";
             this.Load += new System.EventHandler(this.EditForm_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editFormBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.editFormBindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -78,5 +103,8 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.BindingSource editFormBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource editFormBindingSource1;
     }
 }
--- IwM/EditForm.cs
+++ IwM/EditForm.cs
@@ -15,6 +15,7 @@ namespace IwM
     {
         private Patient patient;
         private List<Observation> data;
+        private BindingSource bindingSource1 = new BindingSource();
 
         public EditForm(Patient patient, List<Observation> bundle)
         {
@@ -22,13 +23,29 @@ namespace IwM
             this.patient = patient;
             this.data = bundle;
         }
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e) {
+            string id = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            string column = dataGridView1.Columns[e.ColumnIndex].Name.ToString();
+            string value = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+
+            Console.WriteLine(id.ToString());
+            Console.WriteLine(column.ToString());
+            Console.WriteLine(value);
+        }
         private void EditForm_Load(object sender, EventArgs e)
         {
             string patientName = patient.Id + " " + patient.Name[0].Given.FirstOrDefault() + " " + patient.Name.First().Family;
             titleLabel.Text = "Edycja danych pacjenta: " + patientName;
+            DataTable _myDataTable = new DataTable();
+
+            _myDataTable.Columns.Add("Id");
+            _myDataTable.Columns.Add("Effective");
+            _myDataTable.Columns.Add("Status");
+            _myDataTable.Columns.Add("Interpretation");
 
             foreach (var obs in data)
             {
+                DataRow row = _myDataTable.NewRow();
                 var performer="";
                 try
                 {
@@ -38,8 +55,28 @@ namespace IwM
                 var effective = obs.Effective;
                 var status = obs.Status;
                 var interpretation = obs.Interpretation;
+                row[0] = obs.Id;
+                row[1] = effective;
+                row[2] = status;
+                row[3] = interpretation;
+                _myDataTable.Rows.Add(row);
             }
+
+            bindingSource1.DataSource = _myDataTable;
+            dataGridView1.DataSource = bindingSource1;
+
         }
 
+    }
+}
+
+public class a
+{
+    string one;
+    int two;
+    public a(string o, int t)
+    {
+        this.one = o;
+        this.two = t;
     }
 }
